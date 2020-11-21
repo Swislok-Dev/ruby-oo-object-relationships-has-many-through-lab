50f8f0b5880ed8d431c9d43e9c0b384c87c521c1
cmarker <cmarker4@gmail.com>
2020-11-21 14:09:42 -0600

    Done.

--- lib/genre.rb
+++ lib/genre.rb
@@ -13,4 +13,12 @@ class Genre
         @@all
     end
 
+    def songs
+        Song.all.select { |song| song if song.genre == self}
+    end
+
+    def artists
+        Song.all.collect { |song| song.artist }
+    end
+
 end
--- lib/patient.rb
+++ lib/patient.rb
@@ -1,1 +1,28 @@
 
+class Patient
+    attr_accessor :name, :doctor, :appointment
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_appointment(date, doctor)
+        Appointment.new(date, self, doctor)
+    end
+
+    def appointments
+        Appointment.all.select { |appt| appt if appt.patient == self }
+    end
+
+    def doctors
+        Appointment.all.collect { |appt| appt.doctor }
+    end
+
+end
