76062401c98cdace4b8fb046278dee08059c9bc2
cmarker <cmarker4@gmail.com>
2020-11-21 14:09:36 -0600

    commit tree 2858000f22b38e1bb0b17e49c4e6f178adfff78a

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,7 @@
+require "pry"
 
 class Artist
-    attr_accessor :name
+    attr_accessor :name, :genre
 
     @@all = []
 
@@ -13,8 +14,17 @@ class Artist
         @@all
     end
 
+    def songs
+        Song.all.select { |song| song if song.artist == self }
+    end
+
+
     def new_song(song_name, genre)
-        Song.new(song_name, self, genre)
+        Song.new(song_name, self, genre) 
+    end
+
+    def genres
+        Song.all.collect { |song| song.genre if song.artist = self }
     end
 
 end
