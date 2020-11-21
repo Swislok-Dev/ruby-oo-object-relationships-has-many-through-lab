e1dcdce1b9c60b979c92599e9b5f00dcde133a73
cmarker <cmarker4@gmail.com>
2020-11-21 08:57:28 -0600

    Done.

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,1 +1,16 @@
 
+class Genre
+    attr_accessor :name, :artist, :song
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+end
